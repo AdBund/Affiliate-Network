26c1c72f3f061de7bf90146f68d2181ee6b7b766
e <hitLittlePanda@163.com>
2017-03-17 11:45:46 +0800

    init dev

--- affiliate/rest/avazu.py
+++ affiliate/rest/avazu.py
@@ -14,18 +14,19 @@ class Avazu():
         }
         query = {
             'uid': self.api_id,
+            # 'page': 1,
             'sourceid': self.api_token,
         }
         response = requests.request("GET", url, params=query)
-        # return response.text
-        return response.json()
+        return response.text
+        # return response.json()
+
 
 if __name__ == '__main__':
-
     api_id = '18629'
-    # api_token = '23011'# dsp
+    api_token = '23011'  # dsp
     # api_token = '22433'# incentive traffic
     # api_token = '22417'# FB
-    api_token = '22416'# Google Adwords
+    # api_token = '22416'# Google Adwords
     avazu = Avazu(api_id, api_token)
     print(avazu.get_all_offer())
