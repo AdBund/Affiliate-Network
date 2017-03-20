ad8c43625bc06a3ffe22dd5b5fd2f2f666f81def
yuyou <88315203@qq.com>
2017-03-20 15:15:33 +0800

    新建config package

---
+++ affiliate/config/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@author: amigo
+@contact: 88315203@qq.com
+@phone: 15618318407
+@software: PyCharm
+@file: __init__.py.py
+@time: 2017/3/20 下午3:14
+"""
