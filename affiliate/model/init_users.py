77ba49d394101739c00167b7e290512f00796ebc
yuyou <88315203@qq.com>
2017-03-21 20:25:20 +0800

    修改mongodb类

--- affiliate/model/init_users.py
+++ affiliate/model/init_users.py
@@ -14,19 +14,18 @@
 初始化用户
 """
 
-from affiliate.model.mysql_model import AProvider, AApiToken, AAffiliates, AStatistics, db
+from affiliate.model.mongo_model import Provider, ApiToken
 
 if __name__ == '__main__':
-    yeahmobi = AProvider.get_or_create(name="yeahmobi")
-    avazu = AProvider.get_or_create(name='avazu')
-    AApiToken.get_or_create(token='67951dc0eec37c70b7cc33bfb9b1435d',
-                            provider=yeahmobi[0],
-                            username='dsp@jetmobo.com',
-                            password='Ihave2cars$',
-                            userId='71',
-                            mode=True,
-                            )
-    AApiToken.get_or_create(token='23011', provider=avazu[0], username='18629', userId=71,mode=False)
-    AApiToken.get_or_create(token='22433', provider=avazu[0], username='18629', userId=71,mode=False)
-    AApiToken.get_or_create(token='22417', provider=avazu[0], username='18629', userId=71,mode=False)
-    AApiToken.get_or_create(token='22416', provider=avazu[0], username='18629', userId=71,mode=False)
+    yeahmobi = Provider.get_or_create(name="yeahmobi")
+    avazu = Provider.get_or_create(name="avazu")
+    ApiToken.get_or_create(token='67951dc0eec37c70b7cc33bfb9b1435d',
+                           provider_id=str(yeahmobi.id),
+                           username='dsp@jetmobo.com',
+                           password='Ihave2cars$',
+                           user_id='71',
+                           mode=True)
+    ApiToken.get_or_create(token='23011', provider_id=str(avazu.id), username='18629', user_id='71', mode=False)
+    ApiToken.get_or_create(token='22433', provider_id=str(avazu.id), username='18629', user_id='71', mode=False)
+    ApiToken.get_or_create(token='22417', provider_id=str(avazu.id), username='18629', user_id='71', mode=False)
+    ApiToken.get_or_create(token='22416', provider_id=str(avazu.id), username='18629', user_id='71', mode=False)
--- affiliate/model/mongo_model.py
+++ affiliate/model/mongo_model.py
@@ -4,10 +4,45 @@ from affiliate.model.config import mongodb
 connect(mongodb['name'], host=mongodb['host'], port=int(mongodb['port']))
 
 
-class MGAAffiliates(Document):
-    provider_id = IntField(required=True)
-    offer_id = IntField(required=True)
-    date = DateTimeField(required=True)
-    raw = DictField(required=True)
+class Provider(Document):
+    name = StringField(required=True)
 
+    @classmethod
+    def get_or_create(cls, name):
+        obj = cls.objects.filter(name=name)
+        return obj[0] if obj else cls.objects.create(name=name)
+
+
+class ApiToken(Document):
+    mode = BooleanField()
+    token = StringField()
+    username = StringField()
+    password = StringField()
+    user_id = StringField()
+    provider_id = StringField(required=True)
+
+    @classmethod
+    def get_or_create(cls, token, provider_id, username, user_id, mode, password=''):
+        obj = cls.objects.filter(token=token, provider_id=provider_id, username=username, password=password,
+                                 user_id=user_id, mode=mode)
+        return obj[0] if obj else cls.objects.create(token=token, provider_id=provider_id, username=username,
+                                                     password=password,
+                                                     user_id=user_id, mode=mode)
+
+
+class Affiliates(Document):
+    name = StringField()
+    banner = StringField()
+    provider_id = StringField(required=True)
+    api_token = StringField(required=True)
+    carriers = StringField()
+    country = ListField()
+    offer_id = StringField(required=True)
+    payout = StringField(required=True)
+    preview_link = StringField()
+    tracking_link = StringField(required=True, unique=True)
+
+    @classmethod
+    def save_all(cls, data):
+        [cls.objects.create(**item) for item in data]
 
